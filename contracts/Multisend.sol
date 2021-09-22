bb15fabf28a75f4475ccdfe3e0ad60850d7f6789
TepNik <tepelin.nikita@gmail.com>
2021-09-22 18:34:59 +0500

    Added multisend contract

---
+++ contracts/Multisend.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Multisend is AccessControl {
+    modifier onlyAdmin() {
+        require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "Exilon: Sender is not admin");
+        _;
+    }
+
+    constructor() {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+
+    function multisendEth(address[] calldata users) external payable {
+        require(msg.value > 0, "Multisend: No value");
+        require(users.length > 0, "Multisend: No users");
+
+        uint256 totalValue = msg.value;
+        uint256 restAmount = totalValue;
+        uint256 len = users.length;
+        for(uint256 i = 0; i < len; ++i) {
+            uint256 amountToUser;
+            if (i < len - 1) {
+                amountToUser = totalValue / len;
+                restAmount -= amountToUser;
+            } else {
+                amountToUser = restAmount;
+            }
+
+            (bool success, ) = users[i].call{ value: amountToUser }("");
+            require(success, "Multisend: Eth send if failed");
+        }
+    }
+
+    function multisendToken(address token, address[] calldata users, uint256 amount) external {
+        require(amount > 0, "Multisend: No value");
+        require(users.length > 0, "Multisend: No users");
+
+        uint256 totalValue = amount;
+        uint256 restAmount = totalValue;
+        uint256 len = users.length;
+        for(uint256 i = 0; i < len; ++i) {
+            uint256 amountToUser;
+            if (i < len - 1) {
+                amountToUser = totalValue / len;
+                restAmount -= amountToUser;
+            } else {
+                amountToUser = restAmount;
+            }
+
+            IERC20(token).transferFrom(msg.sender, users[i], amountToUser);
+        }
+    }
+}
